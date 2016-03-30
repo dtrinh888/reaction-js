c4b6854b1828faf62644765ca242c4aa3d42ffca
dtrinh888 <dtrinh888@gmail.com>
2016-03-29 22:14:45 -0700

    finished game need styling

--- js/app.js
+++ js/app.js
@@ -2,14 +2,32 @@ var clickedTime;
 var createdTime;
 var reactionTime;
 
-function makeBox() {
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
+function makeBox() {
 	createdTime = Date.now();
 	var time = Math.random();
-
 	time = time * 5000;
-
 	setTimeout(function(){
+		if (Math.random() > 0.5) {
+			document.getElementById('box').style.borderRadius = '100px';
+		} else {
+			document.getElementById('box').style.borderRadius = '0px';
+		}
+		var top = Math.random();
+		top = top*400;
+		var left = Math.random();
+		left = left*400;
+		document.getElementById('box').style.top = top+"px";
+		document.getElementById('box').style.left = left+"px";
+		document.getElementById('box').style.backgroundColor = getRandomColor();
 		document.getElementById('box').style.display = 'block';
 		createdTime = Date.now();
 	}, time);

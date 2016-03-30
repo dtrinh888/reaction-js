b5af9e7ad9f633898f90c54a9115a847deb52893
dtrinh888 <dtrinh888@gmail.com>
2016-03-29 21:46:13 -0700

    display reaction time

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
+var clickedTime;
+var createdTime;
+var reactionTime;
+
 function makeBox() {
+
+	createdTime = Date.now();
 	var time = Math.random();
 
 	time = time * 5000;
 
 	setTimeout(function(){
 		document.getElementById('box').style.display = 'block';
+		createdTime = Date.now();
 	}, time);
 }
 
-
-
-
 document.getElementById('box').onclick = function(){
+	clickedTime = Date.now();
+	reactionTime = (clickedTime - createdTime)/1000;
+	document.getElementById('time').innerHTML = reactionTime;
 	this.style.display = 'none';
 	makeBox();
 };
